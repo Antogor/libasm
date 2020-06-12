01a473aeca2ab858a8cbedd55f8e253bcb63132b
Antonio Garzon iniesta <agarzon-@c1r7s1.42madrid.com>
2020-06-12 12:49:02 +0200

    Mandatory part done

--- ft_read.s
+++ ft_read.s
@@ -1,7 +1,32 @@
 			section .text
 			global	_ft_read
+			extern	___error
 
 _ft_read:
 			mov		rax, 0x2000003
 			syscall
-			ret 
+			cmp		rax, 9			
+			je		bad_fd
+			cmp		rax, 14
+			je		bad_address
+			ret
+
+bad_address:
+			push	rbp
+			mov		rbp, rsp
+			call	___error			; call function ___error and return the errno memory address and save it in rax
+			mov		QWORD [RAX], 14		; set errno to the correcto value of the error, this is the same that *rax = errno, change the content of the memory address
+			mov		rax, -1				; delete the previous value of rax, the errno memmory addres, and set rax to -1
+			mov		rsp, rbp
+			pop		rbp
+			ret
+
+bad_fd:
+			push	rbp
+			mov		rbp, rsp
+			call	___error
+			mov		QWORD [RAX], 9
+			mov		rax, -1
+			mov		rsp, rbp
+			pop		rbp
+			ret
--- ft_write.s
+++ ft_write.s
@@ -1,27 +1,32 @@
 			section	.text
 			global	_ft_write
-			extern	_errno
-			
+			extern	___error
 
 _ft_write:
-			cmp		rdi, 0
-			jz		null
-			mov		rax, 0x2000004
-			syscall
-			cmp		rax, 9
+			mov		rax, 0x2000004		; call to syscall write
+			syscall						; make write
+			cmp		rax, 9				; the value of rax will change depende of the syscall, and test if it is fine or not
 			je		bad_fd
+			cmp		rax, 14
+			je		bad_address
+			ret
+
+bad_address:
+			push	rbp					; push to the stack rbp
+			mov		rbp, rsp			; save in rbp the rbp memory addres
+			call	___error			; call function ___error and return the errno memory address and save it in rax
+			mov		QWORD [RAX], 14		; set errno to the correcto value of the error, this is the same that *rax = errno, change the content of the memory address
+			mov		rax, -1				; delete the previous value of rax, the errno memmory addres, and set rax to -1
+			mov		rsp, rbp			; restart the stack previous to call ___error function
+			pop		rbp					; pop rbp out of the stack
 			ret
 
 bad_fd:
-		;	push	rbp
-		;	mov		rbp, rsp
-			call	_errno
-		;	mov		rsp, rbp
-		;	pop		rbp
-		;	mov		rax, -1
-			ret
-
-null:
+			push	rbp
+			mov		rbp, rsp
+			call	___error
+			mov		QWORD [RAX], 9
 			mov		rax, -1
+			mov		rsp, rbp
+			pop		rbp
 			ret
-
